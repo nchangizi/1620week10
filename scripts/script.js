223745b16eaf16bd97192d9bde4d82ebc00a11b8
Neda <nedachangizi@gmail.com>
2022-11-09 14:10:38 -0800

    obj pracitce

--- scripts/script.js
+++ scripts/script.js
@@ -40,6 +40,39 @@ const person = {
   gender: "male",
   hobbies: ["music", "skiing"],
 };
-console.log(
-  `my name is ${person.name[0]} ${person.name[1]}. I am interested in ${person.hobbies[0]} and ${person.hobbies[1]}`
-);
+
+for (let key in person) {
+  console.log(key);
+  console.log(person[key]);
+}
+// console.log(
+//   `my name is ${person.name[0]} ${person.name[1]}. I am interested in ${person.hobbies[0]} and ${person.hobbies[1]}`
+// );
+// let student1 = {
+//   ID: "A00022",
+//   GPA: 3.0,
+//   program: "CIT",
+// };
+
+// let student2 = {
+//   ID: "A01000",
+//   GPA: 3.1,
+//   program: "CST",
+// };
+
+// let student3 = {
+//   ID: "A00114",
+//   GPA: 3.2,
+//   program: "CIT",
+// };
+// let students = [student1, student2, student3];
+// let counter = 0;
+// // for (let i = 0; i <students.length ; i++)
+// // {if (students[i].program...}
+// for (let item of students) {
+//   if (item.program === "CIT" && item.GPA > 3) {
+//     console.log(item);
+//     counter = counter +1 ;
+//   }
+// }
+// console.log(counter);
